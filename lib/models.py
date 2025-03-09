856dd7b9a9a81abb0cccb35ae56b0ac1190b6f44
KivaaMaggie <kivaamaggie@gmail.com>
2025-03-09 19:39:59 +0300

    initial commit

--- lib/models.py
+++ lib/models.py
@@ -1,10 +1,51 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey, Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from . import Base  
 
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
+class Role(Base):
+    __tablename__ = 'roles'
 
-Base = declarative_base(metadata=metadata)
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String(100), nullable=False)
+
+    auditions = relationship('Audition', backref='role', lazy='dynamic')
+
+    def actors(self):
+        """Returns a list of actors for this role"""
+        return [audition.actor for audition in self.auditions]
+
+    def locations(self):
+        """Returns a list of locations for this role's auditions"""
+        return [audition.location for audition in self.auditions]
+
+    def lead(self):
+        """Returns the first hired actor for the role or a message if not hired"""
+        hired_auditions = [audition for audition in self.auditions if audition.hired]
+        if hired_auditions:
+            return hired_auditions[0].actor
+        return 'no actor has been hired for this role'
+
+    def understudy(self):
+        """Returns the second hired actor for the role or a message if not hired"""
+        hired_auditions = [audition for audition in self.auditions if audition.hired]
+        if len(hired_auditions) > 1:
+            return hired_auditions[1].actor
+        return 'no actor has been hired for understudy for this role'
+
+
+class Audition(Base):
+    __tablename__ = 'auditions'
+
+    id = Column(Integer, primary_key=True)
+    actor = Column(String(100), nullable=False)
+    location = Column(String(100), nullable=False)
+    phone = Column(Integer, nullable=False)
+    hired = Column(Boolean, default=False)
+
+    role_id = Column(Integer, ForeignKey('roles.id'), nullable=False)
+
+    def call_back(self):
+        """Changes the hired status to True (hiring the actor)"""
+        self.hired = True
+        from . import db  
+        db.session.commit()
